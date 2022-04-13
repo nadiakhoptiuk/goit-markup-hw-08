95c52f4cd5ee87b072bce4a22fa5fb1077a80070
Nadia Khoptiuk <holineinadia@gmail.com>
2022-04-13 15:53:25 +0300

    Adds footer styles for 1200px

--- js/menu.js
+++ js/menu.js
@@ -5,9 +5,9 @@
   menuBtnRef.addEventListener('click', () => {
     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-    menuBtnRef.classList.toggle('menu-btn--is-open');
+    menuBtnRef.classList.toggle('is-open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
-    mobileMenuRef.classList.toggle('menu-btn--is-open');
+    mobileMenuRef.classList.toggle('is-open');
   });
 })();
