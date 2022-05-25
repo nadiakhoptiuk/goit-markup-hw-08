bf5e4ce51e38d5a9a8173acf2f2885be338a0bc3
Nadia Khoptiuk <holineinadia@gmail.com>
2022-05-25 22:03:45 +0300

    add option to close modal by click on the backdrop

--- js/modal.js
+++ js/modal.js
@@ -3,13 +3,21 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    backdrop: document.querySelector('.backdrop'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.backdrop.addEventListener('click', onBackdropClick);
 
   function toggleModal() {
     document.body.classList.toggle('modal-open');
     refs.modal.classList.toggle('is-hidden');
+  }
+
+  function onBackdropClick(event) {
+    if (event.currentTarget === event.target) {
+      toggleModal();
+    }
   }
 })();
