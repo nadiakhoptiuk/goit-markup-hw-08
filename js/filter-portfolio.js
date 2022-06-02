02be486ddca9449cd6c0e77664e282fc5af389db
Nadia Khoptiuk <holineinadia@gmail.com>
2022-06-03 01:11:01 +0300

    add handlebar template for portfolio filter /cdnjs

--- js/filter-portfolio.js
+++ js/filter-portfolio.js
@@ -1,149 +1,10 @@
-const portfolio = [
-  {
-    title: 'Технокряк',
-    type: 'Веб-сайт',
-    tags: ['Веб-сайты', 'Все'],
-    mobile1x: './images/project-1-mobile.jpg',
-    mobile2x: './images/project-1-mobile@2x.jpg',
-    mobile3x: './images/project-1-mobile@3x.jpg',
-    tablet1x: './images/project-1-tablet.jpg',
-    tablet2x: './images/project-1-tablet@2x.jpg',
-    tablet3x: './images/project-1-tablet@3x.jpg',
-    desktop1x: './images/project-1-desktop.jpg',
-    desktop2x: './images/project-1-desktop@2x.jpg',
-    desktop3x: './images/project-1-desktop@3x.jpg',
-    desc: 'Ресурс предлагает комплексные предложения с разным уровнем функционала и сервисов. Все это позволит посетителю получить исчерпывающие сведения о компании или частном лице.',
-    alt: 'Картинка проекта Технокряк',
-  },
-  {
-    title: 'Постер New Orlean vs Golden Star',
-    type: 'Дизайн',
-    tags: ['Дизайн', 'Все'],
-    mobile1x: './images/project-2-mobile.jpg',
-    mobile2x: './images/project-2-mobile@2x.jpg',
-    mobile3x: './images/project-2-mobile@3x.jpg',
-    tablet1x: './images/project-2-tablet.jpg',
-    tablet2x: './images/project-2-tablet@2x.jpg',
-    tablet3x: './images/project-2-tablet@3x.jpg',
-    desktop1x: './images/project-2-desktop.jpg',
-    desktop2x: './images/project-2-desktop@2x.jpg',
-    desktop3x: './images/project-2-desktop@3x.jpg',
-    desc: 'Ресурс предлагает комплексные предложения с разным уровнем функционала и сервисов. Все это позволит посетителю получить исчерпывающие сведения о компании или частном лице.',
-    alt: 'Картинка проекта Постер New Orlean vs Golden Star',
-  },
-  {
-    title: 'Ресторан Seafood',
-    type: 'Приложение',
-    tags: ['Приложения', 'Все'],
-    mobile1x: './images/project-3-mobile.jpg',
-    mobile2x: './images/project-3-mobile@2x.jpg',
-    mobile3x: './images/project-3-mobile@3x.jpg',
-    tablet1x: './images/project-3-tablet.jpg',
-    tablet2x: './images/project-3-tablet@2x.jpg',
-    tablet3x: './images/project-3-tablet@3x.jpg',
-    desktop1x: './images/project-3-desktop.jpg',
-    desktop2x: './images/project-3-desktop@2x.jpg',
-    desktop3x: './images/project-3-desktop@3x.jpg',
-    desc: 'Ресурс предлагает комплексные предложения с разным уровнем функционала и сервисов. Все это позволит посетителю получить исчерпывающие сведения о компании или частном лице.',
-    alt: 'Картинка проекта Ресторан Seafood',
-  },
-  {
-    title: 'Проект Prime',
-    type: 'Маркетинг',
-    tags: ['Маркетинг', 'Все'],
-    mobile1x: './images/project-4-mobile.jpg',
-    mobile2x: './images/project-4-mobile@2x.jpg',
-    mobile3x: './images/project-4-mobile@3x.jpg',
-    tablet1x: './images/project-4-tablet.jpg',
-    tablet2x: './images/project-4-tablet@2x.jpg',
-    tablet3x: './images/project-4-tablet@3x.jpg',
-    desktop1x: './images/project-4-desktop.jpg',
-    desktop2x: './images/project-4-desktop@2x.jpg',
-    desktop3x: './images/project-4-desktop@3x.jpg',
-    desc: 'Ресурс предлагает комплексные предложения с разным уровнем функционала и сервисов. Все это позволит посетителю получить исчерпывающие сведения о компании или частном лице.',
-    alt: 'Картинка проекта Проект Prime',
-  },
-  {
-    title: 'Проект Boxes',
-    type: 'Приложение',
-    tags: ['Приложения', 'Все'],
-    mobile1x: './images/project-5-mobile.jpg',
-    mobile2x: './images/project-5-mobile@2x.jpg',
-    mobile3x: './images/project-5-mobile@3x.jpg',
-    tablet1x: './images/project-5-tablet.jpg',
-    tablet2x: './images/project-5-tablet@2x.jpg',
-    tablet3x: './images/project-5-tablet@3x.jpg',
-    desktop1x: './images/project-5-desktop.jpg',
-    desktop2x: './images/project-5-desktop@2x.jpg',
-    desktop3x: './images/project-5-desktop@3x.jpg',
-    desc: 'Ресурс предлагает комплексные предложения с разным уровнем функционала и сервисов. Все это позволит посетителю получить исчерпывающие сведения о компании или частном лице.',
-    alt: 'Картинка проекта Проект Boxes',
-  },
-  {
-    title: 'Inspiration has no Borders',
-    type: 'Веб-сайт',
-    tags: ['Веб-сайты', 'Все'],
-    mobile1x: './images/project-6-mobile.jpg',
-    mobile2x: './images/project-6-mobile@2x.jpg',
-    mobile3x: './images/project-6-mobile@3x.jpg',
-    tablet1x: './images/project-6-tablet.jpg',
-    tablet2x: './images/project-6-tablet@2x.jpg',
-    tablet3x: './images/project-6-tablet@3x.jpg',
-    desktop1x: './images/project-6-desktop.jpg',
-    desktop2x: './images/project-6-desktop@2x.jpg',
-    desktop3x: './images/project-6-desktop@3x.jpg',
-    desc: 'Ресурс предлагает комплексные предложения с разным уровнем функционала и сервисов. Все это позволит посетителю получить исчерпывающие сведения о компании или частном лице.',
-    alt: 'Картинка проекта Inspiration has no Borders',
-  },
-  {
-    title: 'Издание Limited Edition',
-    type: 'Дизайн',
-    tags: ['Дизайн', 'Все'],
-    mobile1x: './images/project-7-mobile.jpg',
-    mobile2x: './images/project-7-mobile@2x.jpg',
-    mobile3x: './images/project-7-mobile@3x.jpg',
-    tablet1x: './images/project-7-tablet.jpg',
-    tablet2x: './images/project-7-tablet@2x.jpg',
-    tablet3x: './images/project-7-tablet@3x.jpg',
-    desktop1x: './images/project-7-desktop.jpg',
-    desktop2x: './images/project-7-desktop@2x.jpg',
-    desktop3x: './images/project-7-desktop@3x.jpg',
-    desc: 'Ресурс предлагает комплексные предложения с разным уровнем функционала и сервисов. Все это позволит посетителю получить исчерпывающие сведения о компании или частном лице.',
-    alt: 'Картинка проекта Издание Limited Edition',
-  },
-  {
-    title: 'Проект LAB',
-    type: 'Маркетинг',
-    tags: ['Маркетинг', 'Все'],
-    mobile1x: './images/project-8-mobile.jpg',
-    mobile2x: './images/project-8-mobile@2x.jpg',
-    mobile3x: './images/project-8-mobile@3x.jpg',
-    tablet1x: './images/project-8-tablet.jpg',
-    tablet2x: './images/project-8-tablet@2x.jpg',
-    tablet3x: './images/project-8-tablet@3x.jpg',
-    desktop1x: './images/project-8-desktop.jpg',
-    desktop2x: './images/project-8-desktop@2x.jpg',
-    desktop3x: './images/project-8-desktop@3x.jpg',
-    desc: 'Ресурс предлагает комплексные предложения с разным уровнем функционала и сервисов. Все это позволит посетителю получить исчерпывающие сведения о компании или частном лице.',
-    alt: 'Картинка проекта Проект LAB',
-  },
-  {
-    title: 'Growing Business',
-    type: 'Приложение',
-    tags: ['Приложения', 'Все'],
-    mobile1x: './images/project-9-mobile.jpg',
-    mobile2x: './images/project-9-mobile@2x.jpg',
-    mobile3x: './images/project-9-mobile@3x.jpg',
-    tablet1x: './images/project-9-tablet.jpg',
-    tablet2x: './images/project-9-tablet@2x.jpg',
-    tablet3x: './images/project-9-tablet@3x.jpg',
-    desktop1x: './images/project-9-desktop.jpg',
-    desktop2x: './images/project-9-desktop@2x.jpg',
-    desktop3x: './images/project-9-desktop@3x.jpg',
-    desc: 'Ресурс предлагает комплексные предложения с разным уровнем функционала и сервисов. Все это позволит посетителю получить исчерпывающие сведения о компании или частном лице.',
-    alt: 'Картинка проекта Growing Business',
-  },
-];
+import { portfolio } from './portfolio.js';
+
+const source = document.getElementById('text-template').innerHTML;
+
+// initialing function template
+const template = Handlebars.compile(source);
+// execute the compiled template and print the output to the console
 
 const refs = {
   buttons: document.querySelector('.js-buttons'),
@@ -186,8 +47,8 @@ function removeActiveClassFromPrevButton() {
 // create html markup and add it to DOM
 function createPortfolioItemMarkup(array) {
   const markup = array
-    .map(
-      ({
+    .map(portfolio => {
+      const {
         desc,
         title,
         mobile1x,
@@ -201,57 +62,12 @@ function createPortfolioItemMarkup(array) {
         desktop3x,
         type,
         alt,
-      }) => {
-        return /*html*/ `
-  <li class="projects__item">
-    <a href="" class="projects__link">
-      <div class="projects__thumb">
-      <picture>
-        <source
-          srcset="
-            ${mobile1x} 1x,
-            ${mobile2x} 2x,
-            ${mobile3x} 3x
-          "
-          media="(max-width: 767px)"
-        />
-        <source
-          srcset="
-            ${tablet1x} 1x,
-            ${tablet2x} 2x,
-            ${tablet3x} 3x
-          "
-          media="(min-width: 768px) and (max-width: 1199px)"
-        />
-        <source
-          srcset="
-            ${desktop1x} 1x,
-            ${desktop2x} 2x,
-            ${desktop3x} 3x
-          "
-          media="(min-width: 1200px)"
-        />
-        <img
-          class="projects__img"
-          src=${mobile1x}
-          alt=${alt}
-        />
-      </picture>
-      <p class="hidden-text">
-        ${desc}
-      </p>
-      </div>
-
-      <div class="projects__info">
-        <h2 class="projects__title">${title}</h2>
-        <p class="projects__category">${type}</p>
-      </div>
-    </a>
-  </li>
-  `;
-      }
-    )
+      } = portfolio;
+      // call function template for every object
+      return template(portfolio);
+    })
     .join('');
 
-  refs.projects.innerHTML = markup;
+  refs.projects.innerHTML = '';
+  refs.projects.insertAdjacentHTML('beforeend', markup);
 }
